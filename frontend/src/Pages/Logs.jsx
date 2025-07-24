ccdcacde5bcad5232a286c664dc814c6eb39b923
Mohammad Kaif <mkaif1617@gmail.com>
2025-07-25 02:27:35 +0530

    chore: change frontend url for testing only

--- frontend/src/Pages/Logs.jsx
+++ frontend/src/Pages/Logs.jsx
@@ -4,7 +4,10 @@ import TableComponent from '../components/TableComponent';
 // NEW: Import the refresh icon
 import { RefreshCw } from 'lucide-react';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const API_BASE_URL =
+  import.meta.env.MODE === 'production'
+    ? 'https://mini-datadog.onrender.com'
+    : (import.meta.env.VITE_API_URL || 'http://localhost:3000');
 
 function Logs() {
   const [logs, setLogs] = useState([]);
